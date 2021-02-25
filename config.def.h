7ac6f142216bbbed9ccee028ec4bc1e62e7d5d33
parmariza <parmariza0212@gmail.com>
2021-02-25 19:53:39 +0700

    dwm

--- config.def.h
+++ config.def.h
@@ -20,14 +20,19 @@ static const char col_cyan[]        = "#005577";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg         bg         border     float */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan, col_cyan },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
+
+static const char *const autostart[] = {
+	"st", NULL,
+	NULL /* terminate */
 };
 
 /* tagging */
--- config.h
+++ config.h
@@ -1,12 +1,15 @@
 /* See LICENSE file for copyright and license */
 
+/* imports */
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 14;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 14;       /* vert inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 14;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,19 +19,24 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#808080";
-static const char col_border[]      = "#cfddff";
+static const char col_cyan[]        = "#4b70b4";
+static const char col_border[]      = "#daf1ef";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][4]      = {
-	/*               fg         bg         border     float */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_border, col_cyan  },
+	/*               fg         bg         border       float */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2,   col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border,  col_cyan },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
+
+static const char *const autostart[] = {
+	"picom", "-b", NULL,
+	NULL /* terminate */
 };
 
 /* tagging */
@@ -39,10 +47,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "pavucontrol", NULL,	  NULL,		  0,			1,			 1 },
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            0,           -1 },
+    { "Firefox",        NULL,       NULL,       1 << 8,       0,            0 },
+	{ "pavucontrol",    NULL,	    NULL,		0,			  1,		    1 },
 };
 
 /* layout(s) */
@@ -66,22 +74,32 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/zsh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/* brightness control */
+static const char *brightnessup[] = { "brightnessctl", "set", "3%+", NULL };
+static const char *brightnessdown[] = { "brightnessctl", "--min-value=100", "set", "3%-", NULL };
+
+/* volume control */
+static const char *volumeup[] = { "pamixer", "--unmute", "--increase", "3", NULL };
+static const char *volumedown[] = { "pamixer", "--decrease", "3", NULL };
+static const char *volumemute[] = { "pamixer", "--mute", NULL };
+
 /* custom shorcut commands */
-static const char *file_manager[] = { "dolphin", NULL };
+static const char screencapture[] = "scrot -q 100 ~/Pictures/Screencapture/%Y-%m-%d-%H.%M-screenshot.png";
+static const char *file_manager[] = { "thunar", NULL };
 static const char *qb[] = { "qutebrowser", NULL };
 static const char *emacs[] = { "emacs", NULL };
 static const char *pavucontrol[] = { "pavucontrol", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -112,8 +130,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_p,      setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_p,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -130,11 +148,16 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ 0,    						XK_Print,  spawn,          SHCMD("scrot -p -q 100 ~/Pictures/%Y-%m-%d-%H.%M-screenshot.png") },
+	{ MODKEY|ShiftMask,    			XK_End,    spawn,          SHCMD(screencapture) },
     { MODKEY|ShiftMask,             XK_d,      spawn,          {.v = file_manager } },
 	{ MODKEY|ShiftMask, 			XK_e,	   spawn,		   {.v = emacs } },
     { MODKEY|ShiftMask,             XK_f,      spawn,          {.v = qb } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = pavucontrol } },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = pavucontrol } },
+	{ 0,          XF86XK_MonBrightnessUp,      spawn,          {.v = brightnessup } },
+	{ 0,          XF86XK_MonBrightnessDown,    spawn,          {.v = brightnessdown } },
+	{ 0,          XF86XK_AudioRaiseVolume,     spawn,          {.v = volumeup } },
+	{ 0,          XF86XK_AudioLowerVolume,     spawn,          {.v = volumedown } },
+	{ 0,          XF86XK_AudioMute,            spawn,          {.v = volumemute } },
 };
 
 /* button definitions */
